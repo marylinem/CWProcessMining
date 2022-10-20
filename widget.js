f2c415fe3ada5ed5ac6da9b69ea7d7d29cf95bab
Adrian Hendrik Schwaiger <adrianhendrik.schwaiger@exxeta.com>
2022-10-20 14:50:11 +0200

    dbg

--- widget.js
+++ widget.js
@@ -62,40 +62,40 @@
         var namespace = joint.shapes;
 
 
-        var graph = new joint.dia.Graph({}, { cellNamespace: namespace });
+        // var graph = new joint.dia.Graph({}, { cellNamespace: namespace });
 
-        var paper = new joint.dia.Paper({
-            el: document.getElementById('diagram-container'),
-            model: graph,
-            width: 600,
-            height: 600,
-            gridSize: 1,
-            cellViewNamespace: namespace
-        });
+        // var paper = new joint.dia.Paper({
+        //     el: document.getElementById('diagram-container'),
+        //     model: graph,
+        //     width: 600,
+        //     height: 600,
+        //     gridSize: 1,
+        //     cellViewNamespace: namespace
+        // });
 
-        var rect = new joint.shapes.standard.Rectangle();
-        rect.position(100, 30);
-        rect.resize(100, 40);
-        rect.attr({
-            body: {
-                fill: 'blue'
-            },
-            label: {
-                text: 'Hello',
-                fill: 'white'
-            }
-        });
-        rect.addTo(graph);
+        // var rect = new joint.shapes.standard.Rectangle();
+        // rect.position(100, 30);
+        // rect.resize(100, 40);
+        // rect.attr({
+        //     body: {
+        //         fill: 'blue'
+        //     },
+        //     label: {
+        //         text: 'Hello',
+        //         fill: 'white'
+        //     }
+        // });
+        // rect.addTo(graph);
 
-        var rect2 = rect.clone();
-        rect2.translate(300, 0);
-        rect2.attr('label/text', 'World!');
-        rect2.addTo(graph);
+        // var rect2 = rect.clone();
+        // rect2.translate(300, 0);
+        // rect2.attr('label/text', 'World!');
+        // rect2.addTo(graph);
 
-        var link = new joint.shapes.standard.Link();
-        link.source(rect);
-        link.target(rect2);
-        link.addTo(graph);
+        // var link = new joint.shapes.standard.Link();
+        // link.source(rect);
+        // link.target(rect2);
+        // link.addTo(graph);
     }
     document.head.appendChild(scriptJoint);
     customElements.define("com-demo-jointjs", JointJS);
