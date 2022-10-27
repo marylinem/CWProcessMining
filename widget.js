8493c627d7488ce778dff97e655f93cdd1fa3826
Adrian Hendrik Schwaiger <adrianhendrik.schwaiger@exxeta.com>
2022-10-27 11:47:25 +0200

    cleanup

--- widget.js
+++ widget.js
@@ -39,30 +39,6 @@
     }
 
 
-    function getOutgoingEdges(n, edges) {
-        const outgoing = [];
-        for (const [key, e] of edges.entries()) {
-            if (e.n0 == n) outgoing.push({ key: key, n0: e.n0, n1: e.n1 });
-        }
-        return outgoing;
-    }
-
-    function hasIncomingEdges(n, edges) {
-        const incoming = [];
-        for (const [key, e] of edges.entries()) {
-            if (e.n1 == n) return true;
-        }
-        return false;
-    }
-
-    function isIndependent(prevNodes, node, edges) {
-        for (const [key, e] of edges.entries()) {
-            for (const n of prevNodes) {
-                if (e.n1 == node && e.n0 == n) return false;
-            }
-        }
-        return true;
-    }
 
     function strComp(strA, strB) {
         return strA < strB ? 1 : strA > strB ? -1 : 0;
@@ -74,50 +50,6 @@
     class JointJS extends HTMLElement {
 
 
-
-        /* 
-        * Do a topological sort on nodes map
-        * nodes map<string, string>: key: <id>, value: <label>
-        * edges map<string, object>: key: <>, value: {n0, n1, value}; n0 - node with outgoing edge e
-        * 
-        */
-        topologicalSort(nodes, edges) {
-            const newNodes = [];
-            const beginningNodes = [];
-            // Get all beginning nodes
-            for (const [n, label] of nodes.entries()) {
-                let isBeginning = true;
-                for (const e of edges.values()) {
-                    if (e.n1 == n) {
-                        isBeginning = false;
-                        break;
-                    }
-                }
-                if (isBeginning) beginningNodes.push({ id: n, label: label });
-            }
-            const edgeCopy = new Map(edges);
-            // Topological Sorting with Kahn's algorithm
-            while (beginningNodes.length > 0) {
-                const n = beginningNodes.pop();
-                newNodes.push({ id: n.id, label: n.label })
-                const outgoing = getOutgoingEdges(n.id, edgeCopy);
-                for (const e of outgoing) {
-                    const m = e.n1;
-                    edgeCopy.delete(e.key);
-                    if (!hasIncomingEdges(m, edgeCopy)) beginningNodes.push({ id: m, label: nodes.get(m) });
-                }
-            }
-            if (edgeCopy.size > 0) { // no topological sorting possible, sort by id
-                console.log("No topological sort possible")
-                newNodes = [];
-                for (const [n, label] of nodes) {
-                    newNodes.push({ id: n, label: label });
-                }
-                newNodes.sort((a, b) => strComp(a.id, b.id));
-            }
-            return newNodes;
-        }
-
         clearGraph() {
             this.graph.clear();
             this.nodes = new Map();
@@ -126,26 +58,12 @@
 
         constructGraph() {
             console.log("Drawing Graph")
-            let px = 10;
-            let py = 10;
+
             let nodeMap = new Map();
 
-            let sortedNodes = this.topologicalSort(this.nodes, this.relations)
-            let prevNodes = [];
 
-            sortedNodes.forEach(n => {
+            this.nodes.forEach((n, k) => {
                 let rect = new joint.shapes.standard.Rectangle();
-
-                if (isIndependent(prevNodes, n.id, this.relations)) {
-                    px += 150;
-                }
-                else {
-                    py += 50;
-                    px = 10;
-                    prevNodes = [];
-                }
-                prevNodes.push(n.id);
-                rect.position(px, py);
                 rect.resize(130, 40);
 
                 rect.attr({
@@ -153,16 +71,15 @@
                         fill: 'blue'
                     },
                     label: {
-                        text: n.label,
+                        text: n,
                         fill: 'white'
                     }
                 });
                 rect.addTo(this.graph);
-                nodeMap.set(n.id, rect);
+                nodeMap.set(k, rect);
             })
 
             this.relations.forEach((r, v) => {
-                console.log({ r, v });
                 var link = new joint.shapes.standard.Link();
                 link.source(nodeMap.get(r.n0));
                 link.target(nodeMap.get(r.n1));
@@ -172,6 +89,7 @@
                             text: "" + r.val
                         },
                         strokeLinejoin: 'round',
+                        strokeLinecap: 'round',
                     }
                 });
                 link.addTo(this.graph);
@@ -210,7 +128,6 @@
             data.forEach(row => {
                 let process = row.dimensions_0;
                 let relation = row.dimensions_1;
-                let filter = row.dimensions_3 || undefined;
                 if (curRelationId == relation.id) {
                     let key = prevProcessData.id + "_" + process.id;
                     let val = 0;
