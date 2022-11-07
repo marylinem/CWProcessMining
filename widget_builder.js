117420058d59f5f11d00fefe161d5088d5e44641
Mengoualeu Majiade <mame1@MN-EX1377.local>
2022-11-07 17:52:09 +0100

    css added

--- widget_builder.js
+++ widget_builder.js
@@ -7,28 +7,32 @@
     <br>
     <br>
     <label for="selMeasure">Select Measure<br></label>
+    <div class="selectdiv">
     <select id="selMeasure">
         <option>--NONE--</option>
     </select>
-    <br>
+    </div>
     <br>
     <label for="selDim0">Select Events<br></label>
+    <div class="selectdiv">
     <select id="selDim0">
         <option>--NONE--</option>
     </select>
-    <br>
+    </div>
     <br>
     <label for="selDim1">Select Relation<br></label>
+    <div class="selectdiv">
     <select id="selDim1">
         <option>--NONE--</option>
     </select>
-    <br>
+    </div>
     <br>
     <label for="selDim2">Select Timestamp<br></label>
+    <div class="selectdiv">
     <select id="selDim2">
         <option>--NONE--</option>
     </select>
-    <br>
+    </div>
     <br>
     <button id="createModel" type="button">Apply</button>
     <br>
@@ -68,6 +72,41 @@
         line-height: 1.75;
         margin-left: 0.25rem;
     }
+    .selectdiv {
+        position: relative;  
+        width: 90%;
+        padding: 0 2rem 0 0;
+        background-color: #fff;
+        border-radius: 0;
+        height: 2rem;
+        margin-left: 0.25rem;
+      }
+      
+      
+      select::-ms-expand {
+      display: none;
+      }
+      
+      .selectdiv select {
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;        
+        display: block;
+        width: 100%;
+        height: 30px;
+        float: right;
+        margin: 5px 0px;
+        padding: 0px 24px;
+        font-size: 16px;
+        line-height: 1.75;
+        color: #333;
+        background-color: #ffffff;
+        background-image: none;
+        border: 1px solid #cccccc;
+        -ms-word-break: normal;
+        word-break: normal;
+        
+      }
     </style>
     `;
     class JointJSBuilderPanel extends HTMLElement {
